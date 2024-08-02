ef1c7251d33e767f1e4216f88463c0fb1537fc2f
kris-king007 <kris.king007@icloud.com>
2024-08-01 22:32:19 -0400

    Ripped out graphQL, mongo, and apollo. Deleted all Constituent entity files.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,9 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConstituentsModule } from './constituents/constituents.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { join } from 'path';
 
 @Module({
-  imports: [
-    ConstituentsModule,
-    MongooseModule.forRoot('mongodb://loßcalhost/nest'),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      typePaths: ['./**/*.graphql'],
-      path: join(process.cwd(), 'src/graphql.schema.ts'),
-      installSubscriptionHandlers: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/constituents/constituents.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ConstituentsService } from './constituents.service';
-import { ConstituentsResolver } from './constituents.resolver';
-
-@Module({
-  providers: [ConstituentsResolver, ConstituentsService],
-})
-export class ConstituentsModule {}
